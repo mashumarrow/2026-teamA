fd74ad976dd308795c7e1b5d4828ec78d5d75946
mashumarrow <134787738+mashumarrow@users.noreply.github.com>
2026-07-08 11:01:45 +0900

    fix : set up

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -8,10 +8,18 @@ until PGPASSWORD=$DATABASE_PASSWORD psql -h "$DATABASE_HOST" -U "$DATABASE_USER"
 done
 
 echo 'PostgreSQL started'
+echo 'Checking and installing gems...'
+bundle install
+
 
 # マイグレーション実行
 bundle exec rails db:create --quiet 2>/dev/null || true
 bundle exec rails db:migrate --quiet
 
-# コンテナをデタッチモードで実行する場合の処理
-exec "$@"
+# サーバーの競合を防ぐため過去の pid ファイルが残っていたら削除する処理
+if [ -f /app/tmp/pids/server.pid ]; then
+  rm -f /app/tmp/pids/server.pid
+fi
+
+echo 'Starting Rails server...'
+exec bundle exec rails s -b '0.0.0.0'
