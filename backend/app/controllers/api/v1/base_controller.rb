c3533a031fc5061934dde5966177f96db7bed36f
mashumarrow <134787738+mashumarrow@users.noreply.github.com>
2026-07-17 12:57:48 +0900

    delete basic auth

--- backend/app/controllers/api/v1/base_controller.rb
+++ backend/app/controllers/api/v1/base_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class BaseController < ApplicationController
       skip_before_action :verify_authenticity_token
-      skip_before_action :authenticate_basic_auth_if_enabled
+      
 
       private
 
--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,20 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_basic_auth_if_enabled
   helper_method :current_user, :logged_in?
 
   private
-
-  def authenticate_basic_auth_if_enabled
-    username = ENV.fetch("BASIC_AUTH_USERNAME", nil)
-    password = ENV.fetch("BASIC_AUTH_PASSWORD", nil)
-    return if username.blank? || password.blank?
-
-    authenticate_or_request_with_http_basic("Room Portal") do |given_username, given_password|
-      ActiveSupport::SecurityUtils.secure_compare(given_username, username) &
-        ActiveSupport::SecurityUtils.secure_compare(given_password, password)
-    end
-  end
 
   def current_user
     return @current_user if defined?(@current_user)
